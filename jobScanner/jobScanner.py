7f6b5d2eb4bc5e1196c7cb9537d8a1c59c86cb18
Carl <carlylespeaks@gmail.com>
2020-08-24 16:29:50 -0700

    update checklist notes

--- jobScanner/jobScanner.py
+++ jobScanner/jobScanner.py
@@ -14,7 +14,7 @@ Workflow:
 
 Setup:
  - [x]  CSV file with Skills and ranks
- - [ ]  File with job description (or user copy paste description)
+ - [x]  File with job description (or user copy paste description)
  
  Resume in application folder
  - [ ]  - Run resume ingester (load and convert resume to usable data structure
@@ -22,9 +22,9 @@ Setup:
 Prepare job description for loading:
  - [ ]  Remove non alpha
  - [x]  Lowercase all
- - [ ]  Load job description into list
- - [ ]  Count skills, using skills.csv for the skills to look for
- - [ ]  Pass or Fail job description
+ - [x]  Load job description into list
+ - [x]  Count skills, using skills.csv for the skills to look for
+ - [x]  Pass or Fail job description
 
 Pass: go to responder module
 Fail: Give user option to try another job or respond anyway
@@ -33,13 +33,13 @@ Fail: Give user option to try another job or respond anyway
 Task list:
 
  - [ ]  Figure out data structure for resume
- - [ ]  Figure out data structure for job description
- - [ ]  CODE read skill ratings into variables from a CSV source
+ - [x]  Figure out data structure for job description
+ - [x]  CODE read skill ratings into variables from a CSV source
  - [ ]  CODE Read resume into data structure
 >Need dates, companies and words list (work history with skills data)
- - [ ]  CODE: Read Job Description into data structure
- - [ ]  CODE: Rank job vs skills
- - [ ]  CODE: If pass: get skills, dates & comps from resume
+ - [x]  CODE: Read Job Description into data structure
+ - [x]  CODE: Rank job vs skills
+ - [x]  CODE: If pass: get skills, dates & comps from resume
 
 FILES:
 
